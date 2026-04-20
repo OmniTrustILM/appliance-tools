964bbcf60d9d6705352c30e56bff28be3ec15c34
Jan Tomasek <jan@tomasek.cz>
2026-04-20 16:27:46 +0200

    Revert "Merge pull request #143 from semik/2.17.0"
    This reverts commit 8e26cea7b144368f0dac2a3455a6393f3b250384, reversing
    changes made to 4aac4f23f1d53f796d9908c7af1547aae2e3d74d.

--- build-deb.sh
+++ build-deb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # test if repository was correctly initialized, if no initialize
-if [ ! -e etc/ilm-ansible/roles/ilm/tasks/main.yml ]
+if [ ! -e etc/czertainly-ansible/roles/czertainly/tasks/main.yml ]
 then
     git submodule update --init --recursive
 fi
 
-install='debian/ilm-appliance-tools.install'
+install='debian/install'
 echo -n "Creating $install: "
-(find ./etc -type f; find ./usr \( -type f -o -type l \)) |\
+(find ./etc -type f; find ./usr -type f) |\
     grep -v \~\$| grep -v \.git | grep -v \.travis > $install
 echo "done."
 
@@ -16,9 +16,10 @@ cp -f LICENSE debian/copyright
 
 dpkg-buildpackage -b -us -uc
 
+name=`cat debian/files |grep \.deb | sed 's/ .*$//'`
+
 # https://pmhahn.github.io/debian-oot-build/
-for name in `cat debian/files | grep \.deb | sed 's/ .*$//'`; do
-    echo "moving package file $name to current directory"
-    mv -f "../$name" .
-done
+echo "moving package file $name to current directory"
+mv -f "../$name" .
 ls -l *deb
+
