f7e06ded4309e5eebd09035d8475c0d92896ec29
Jan Tomášek <jan@tomasek.cz>
2026-04-20 17:59:41 +0200

    version 2.17.0 (#145) (#146)

--- build-deb.sh
+++ build-deb.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # test if repository was correctly initialized, if no initialize
-if [ ! -e etc/czertainly-ansible/roles/czertainly/tasks/main.yml ]
+if [ ! -e etc/ilm-ansible/roles/ilm/tasks/main.yml ]
 then
     git submodule update --init --recursive
 fi
 
-install='debian/install'
+install='debian/ilm-appliance-tools.install'
 echo -n "Creating $install: "
-(find ./etc -type f; find ./usr -type f) |\
+(find ./etc -type f; find ./usr \( -type f -o -type l \)) |\
     grep -v \~\$| grep -v \.git | grep -v \.travis > $install
 echo "done."
 
@@ -16,10 +16,9 @@ cp -f LICENSE debian/copyright
 
 dpkg-buildpackage -b -us -uc
 
-name=`cat debian/files |grep \.deb | sed 's/ .*$//'`
-
 # https://pmhahn.github.io/debian-oot-build/
-echo "moving package file $name to current directory"
-mv -f "../$name" .
+for name in `cat debian/files | grep \.deb | sed 's/ .*$//'`; do
+    echo "moving package file $name to current directory"
+    mv -f "../$name" .
+done
 ls -l *deb
-
